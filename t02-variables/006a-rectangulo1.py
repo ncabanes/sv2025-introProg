1012040d761102620cf3da703ce8ab6145fb11ba
Nacho Cabanes <ncabanes@users.noreply.github.com>
2025-09-20 13:32:41 +0200

    006 - Datos numéricos, superficie de rectángulo

---
+++ t02-variables/006a-rectangulo1.py
@@ -0,0 +1,11 @@
+# Contacto con "input" y datos enteros (versión 1)
+# Por Nacho
+
+print("Dime la base del rectángulo")
+base = int ( input() )
+
+print("Dime la altura del rectángulo")
+altura = int ( input() )
+
+superficie = base * altura
+printf("La superficie es", superficie)
---
+++ t02-variables/006b-rectangulo2.py
@@ -0,0 +1,6 @@
+# Contacto con "input" y datos enteros (versión 2)
+# Por Nacho
+
+base=int(input("Dime la base del rectángulo "))
+altura=int(input("Dime la altura del rectángulo "))
+print("La superficie es", base * altura)
