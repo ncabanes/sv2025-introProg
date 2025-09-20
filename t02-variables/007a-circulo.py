b9d52f0c041f0c583e85e0a7b3830e8084ab2329
Nacho Cabanes <ncabanes@users.noreply.github.com>
2025-09-20 13:45:30 +0200

    007 - Datos reales (círculo, cono)

---
+++ t02-variables/007a-circulo.py
@@ -0,0 +1,6 @@
+# Pide al usuario el radio de un círculo 
+# y muestra su superficie 
+# (su valor será 3.14 por el radio al cuadrado)
+
+radio = float(input("Dime el radio: "))
+print("El área del círculo es :", 3.14*radio**2)
---
+++ t02-variables/007b-cono.py
@@ -0,0 +1,7 @@
+# Pide al usuario el radio de la base de un cono 
+# y su altura (que quizá tengan decimales). 
+# Muestra el volumen del cono.
+
+radio = float(input("Dime el radio del cono: "))
+altura = float(input("Dime la altura del cono: "))
+print("El volumen del cono es:", 3.14*radio**2 * altura / 3)
