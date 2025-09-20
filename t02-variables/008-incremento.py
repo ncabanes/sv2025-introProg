aa0d16d90e8644ec964b89d23a0d8ef31508de70
Nacho Cabanes <ncabanes@users.noreply.github.com>
2025-09-20 14:00:39 +0200

    008 - Incrementar una  variable

---
+++ t02-variables/008-incremento.py
@@ -0,0 +1,8 @@
+# Pregunta al usuario su edad (por ejemplo, puede responder con "17"). 
+# Deberás responderle diciendo que parece que tenga 2 años más 
+# (por ejemplo, "Parece que tengas 19".
+
+edad = int(input("Dime tu edad: "))
+# edad = edad + 2
+edad += 2
+print("Pues parece que tengas", edad)
