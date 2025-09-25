9dedc50b2ce4ed898d8c18c9a89601d2fefdf0ea
Nacho Cabanes <ncabanes@users.noreply.github.com>
2025-09-25 18:11:07 +0200

    013 - Es múltiplo de otro?

---
+++ t03-condiciones/013-esMultiplo.py
@@ -0,0 +1,8 @@
+# Es un número múltiplo de otro?
+
+n1 = int(input("Dime el primer numero: "))
+n2 = int(input("Dime el segundo numero: "))
+if n1 % n2 == 0 :
+    print("Es multiplo")
+else:
+    print("No es multiplo")
