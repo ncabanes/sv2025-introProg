af26572cfa5b0352f6235512a84e16544b035a18
Nacho Cabanes <ncabanes@users.noreply.github.com>
2025-09-25 18:10:48 +0200

    011 - if + else

---
+++ t03-condiciones/011-ifelse.py
@@ -0,0 +1,9 @@
+## Ejemplo de if + else
+
+pulgadas = float(input("Cuantas pulgadas: "))
+metros = pulgadas * 0.0254
+print("Son:", metros,"metros")
+if metros >= 4.40 :
+    print("No cabe")
+else :
+    print("Sí cabe")
