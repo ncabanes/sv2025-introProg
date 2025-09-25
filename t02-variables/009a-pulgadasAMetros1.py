00bd97559fac17fdc1a84123e2f08af8006ec71b
Nacho Cabanes <ncabanes@users.noreply.github.com>
2025-09-25 18:10:24 +0200

    009 - Pulgadas a metros

---
+++ t02-variables/009a-pulgadasAMetros1.py
@@ -0,0 +1,4 @@
+# Convertir pulgadas a metros
+
+pulgadas = float(input("Cuantas pulgadas: "))
+print("Son:", pulgadas * 0.0254,"metros")
---
+++ t02-variables/009b-pulgadasAMetros2.py
@@ -0,0 +1,5 @@
+# Convertir pulgadas a metros (versión 2, con variable auxiliar)
+
+pulgadas = float(input("Cuantas pulgadas: "))
+metros = pulgadas * 0.0254
+print("Son:", metros,"metros")
