2ed45d84230ca2a9dcc3f2f9bb92bf04d33cc118
Nacho Cabanes <ncabanes@users.noreply.github.com>
2025-09-25 18:10:37 +0200

    010 - Contacto con "if"

---
+++ t03-condiciones/010-if.py
@@ -0,0 +1,7 @@
+# Cabe el coche? (contacto con "if")
+
+pulgadas = float(input("Cuantas pulgadas: "))
+metros = pulgadas * 0.0254
+print("Son:", metros,"metros")
+if metros >= 4.40 :
+    print("No cabe")
