a58635bcbc8a8f549f14caff4acf85b879897a61
Nacho Cabanes <ncabanes@users.noreply.github.com>
2025-09-30 21:44:22 +0200

    014 - Login

---
+++ t03-condiciones/014a-login1.py
@@ -0,0 +1,13 @@
+# Login
+
+# Crea un programa que pregunte al usuario su nombre. Si coincide 
+# con el tuyo, le dirá algo como "Acceso concedido". Si no es así, 
+# le responderá "No te denegado".
+
+# Versión 1, con el operador ==
+
+nombre = input("Login?")
+if nombre == "Valentino":
+    print ("Acceso concedido")
+else:
+    print ("Acceso denegado")
