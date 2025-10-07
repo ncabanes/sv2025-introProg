fb9737f8b631c81467e6325bae5ad1e7fa1c188c
Nacho Cabanes <ncabanes@users.noreply.github.com>
2025-10-07 17:23:07 +0200

    034 - for, 1 al 10

---
+++ t04-bucles/034-for-1a10.py
@@ -0,0 +1,14 @@
+# Contar del 1 al 10, con "for"
+
+# Comparación: primero con while (todo en una línea)
+i = 1
+while i <= 10:
+    print(i, end=" ")
+    i += 1
+
+# Salto de línea
+print()
+
+# Y la alternativa con "for" (también todo en una línea)
+for i in range(1,11):
+    print(i, end = " ")
