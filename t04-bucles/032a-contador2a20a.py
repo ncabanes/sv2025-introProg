aa028053ae3e27a12f7dc1e15bce3af95a9d2a42
Nacho Cabanes <ncabanes@users.noreply.github.com>
2025-10-07 17:20:14 +0200

    032 - Mostrar pares del 20 al 20, con "while"

---
+++ t04-bucles/032a-contador2a20a.py
@@ -0,0 +1,8 @@
+# Contar del 2 al 20, de 2 en 2, con "while"
+
+# Versión 1, con el planteamiento esperable
+
+i = 2
+while i <= 20:
+    print(i)
+    i += 2
---
+++ t04-bucles/032b-contador2a20b.py
@@ -0,0 +1,9 @@
+# Contar del 2 al 20, de 2 en 2, con "while"
+
+# Versión 2, recorriendo todos y mirando si son pares
+
+i = 1
+while i <= 20:
+    if i % 2 == 0:
+        print(i)
+    i += 1
