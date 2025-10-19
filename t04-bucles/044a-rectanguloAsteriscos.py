6cfdf3c1c79f5c5075edc22a7f6ff6f627b206d0
Nacho Cabanes <ncabanes@users.noreply.github.com>
2025-10-19 21:54:19 +0200

    044- Rectángulo de asteriscos

---
+++ t04-bucles/044a-rectanguloAsteriscos.py
@@ -0,0 +1,20 @@
+# Rectangulo de asteriscos
+
+# Previo: una fila
+
+for i in range(0,5):
+    print("*", end="")
+
+# Espacio en blanco para separar
+
+print()
+print()
+print()
+
+# Y el rectángulo
+
+for fila in range(0,3):
+    for columna in range(0,5):
+        print("*", end="")
+    print()
+
---
+++ t04-bucles/044b-rectanguloAsteriscos-compracto.py
@@ -0,0 +1,7 @@
+# Rectángulo de asteriscos, versión 2
+# Variante avanzada: Python permite multiplicar un texto por un número
+# En la mayoría de lenguajes no existe algo parecido
+
+for fila in range(0,3):
+    print("*" * 5)
+
