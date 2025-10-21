e1baac55044e79e973bf730ed93a4d96e3177774
Nacho Cabanes <ncabanes@users.noreply.github.com>
2025-10-21 19:29:02 +0200

    049 - Función animar (V2: parámetros)

---
+++ t05-funciones/049-animar2.py
@@ -0,0 +1,10 @@
+# Función animar (segunda versión, con parámetros)
+
+def animar(asignatura):
+    print("Voy a sacar un 10 en" + asignatura)
+
+animar("Aplic. Web")
+animar("SOM")
+animar("Programación")
+
+
