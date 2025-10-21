c52385dfc5a0515190a1094b23bebc2c61741578
Nacho Cabanes <ncabanes@users.noreply.github.com>
2025-10-21 19:29:22 +0200

    050 - Función escribirMenorDe

---
+++ t05-funciones/050-mostrarMenorDe.py
@@ -0,0 +1,13 @@
+# Función mostrarMenorDe (muestra el menor de los 
+# dos números que se indiquen como parámetros)
+
+def mostrarMenorDe(n1, n2):
+    if n1 < n2:
+        print(n1)
+    else:
+        print(n2)
+
+
+mostrarMenorDe(5, 7)
+mostrarMenorDe(15, 10)
+mostrarMenorDe(-8, -8)
