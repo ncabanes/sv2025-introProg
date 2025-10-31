24deee847fadb86b751b58dad272b351fdae9a6d
Nacho Cabanes <ncabanes@users.noreply.github.com>
2025-10-31 18:49:12 +0100

    055 - Función mayor_de_3

---
+++ t05-funciones/055b-mayorDe3_2.py
@@ -0,0 +1,15 @@
+# Crea una función "mayor_de_3", que reciba como parámetros tres números reales 
+# y devuelva el valor numérico del mayor de ellos.
+
+# Versión 2: con "type hinting"
+
+def mayor_de_3(n1: float, n2: float, n3: float) -> float:
+    if n1 >= n2 and n1 >= n3:
+        return n1
+    elif n2 >= n1 and n2 >= n3:
+        return n2
+    else:
+        return n3
+
+x = mayor_de_3( 7, 10, -1.5 )
+print(x)
