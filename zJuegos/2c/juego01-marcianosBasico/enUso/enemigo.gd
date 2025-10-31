960da7dff9def4c1693895bf4c1084022794746d
Nacho Cabanes <ncabanes@users.noreply.github.com>
2025-10-31 19:52:46 +0100

    Juego 1, versión 4c - enemigo diagonal + colisiones

--- zJuegos/2c/juego01-marcianosBasico/enUso/enemigo.gd
+++ zJuegos/2c/juego01-marcianosBasico/enUso/enemigo.gd
@@ -1,10 +1,17 @@
 extends Area2D
 
-var velocidad = 400
+var velocidadX = 400
+var velocidadY = 300
 
 func _process(delta: float) -> void:
-	position.x += velocidad * delta
-	if position.x > 1200:
-		velocidad = -velocidad
-	if position.x < 80:
-		velocidad = -velocidad
+	position.x += velocidadX * delta
+	if position.x > 1200 or position.x < 80:
+		velocidadX = -velocidadX
+	
+	position.y += velocidadY * delta
+	if position.y > 640 or position.y < 80:
+		velocidadY = -velocidadY
+
+
+func _on_area_entered(area: Area2D) -> void:
+	print("Boom")
