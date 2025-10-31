4f41b93438da1f243e96552f4dbcbdfcebcb268a
Nacho Cabanes <ncabanes@users.noreply.github.com>
2025-10-31 19:38:22 +0100

    Juego 1, versión 3 - movimiento limitado

--- zJuegos/2c/juego01-marcianosBasico/enUso/nave.gd
+++ zJuegos/2c/juego01-marcianosBasico/enUso/nave.gd
@@ -4,5 +4,7 @@ func _ready() -> void:
 	position.x = 400
 
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_right"):
-		position.x += 2
+	if Input.is_action_pressed("ui_right") and position.x < 1200: 
+		position.x += 4
+	if Input.is_action_pressed("ui_left") and position.x > 80: 
+		position.x -= 4
