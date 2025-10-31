16a5373a05f96797c1578474912cec65eba391ca
Nacho Cabanes <ncabanes@users.noreply.github.com>
2025-10-31 19:40:12 +0100

    Juego 1, versión 5 - enemigo diagonal + colisiones

--- zJuegos/2c/juego01-marcianosBasico/enUso/enemigo.gd
+++ zJuegos/2c/juego01-marcianosBasico/enUso/enemigo.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
 
-var velocidad = 300
+var velocidadX = 300
+var velocidadY = 300
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,6 +11,14 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.x += velocidad * delta
+	position.x += velocidadX * delta
 	if position.x > 1280 or position.x < 0:
-		velocidad = -velocidad
+		velocidadX = -velocidadX
+	
+	position.y += velocidadY * delta
+	if position.y > 720 or position.y < 0:
+		velocidadY = -velocidadY
+
+
+func _on_area_entered(area: Area2D) -> void:
+	print("Boom") # Replace with function body.
