41c7cb9a2987cca8e3579c32d3db44358b3ef409
Nacho Cabanes <ncabanes@users.noreply.github.com>
2025-11-25 18:23:36 +0100

    Marcianos 2 (2º A) - Disparos, parte 1

--- zJuegos/2a/juego02-marcianosMejorado/enUso/scripts/nivel.gd
+++ zJuegos/2a/juego02-marcianosMejorado/enUso/scripts/nivel.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
 var escenaEnemigo : PackedScene
+var escenaDisparo : PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	escenaEnemigo = load("res://escenas/enemigo.tscn")
+	escenaDisparo = load("res://escenas/disparo.tscn")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,3 +16,7 @@ func _process(delta: float) -> void:
 		enemigo.position.x = 1200
 		enemigo.position.y = 360
 		add_child(enemigo)
+		
+		var disparo = escenaDisparo.instantiate()
+		disparo.position = $Nave.position
+		add_child(disparo)
