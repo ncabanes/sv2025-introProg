6ee5f64059a16598d769ea103dfa7482969fdd92
Nacho Cabanes <ncabanes@users.noreply.github.com>
2025-12-12 17:24:32 +0100

    Reto 03: Contando en la arena (3 versiones)

---
+++ zRetos/reto02a-ContandoEnLaArena.py
@@ -0,0 +1,32 @@
+# Reto 02: Contando en la arena
+# Acepta el reto, 369
+# https://aceptaelreto.com/problem/statement.php?id=369
+
+"""
+Entrada
+La entrada estará compuesta por distintos números mayores que 0 y 
+que nunca serán mayores que 1.000, cada uno en una línea.
+La entrada termina con un 0, que no debe procesarse.
+
+Salida
+Para cada número se debe escribir, en una línea independiente, 
+su codificación en base 1.
+
+Entrada de ejemplo
+1
+4
+6
+0
+
+Salida de ejemplo
+1
+1111
+111111
+"""
+
+# Versión 1: Con lectura previa
+
+n = int(input())
+while n != 0:
+	print (n * "1")
+	n = int(input())
