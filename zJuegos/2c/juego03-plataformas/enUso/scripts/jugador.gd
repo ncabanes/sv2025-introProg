4cffb421cf0b372cb2e024f350ef5277cd68b159
Nacho Cabanes <ncabanes@users.noreply.github.com>
2026-01-24 11:52:01 +0100

    Juego plataformas (2º C) - Animaciones, monedas

--- zJuegos/2c/juego03-plataformas/enUso/scripts/jugador.gd
+++ zJuegos/2c/juego03-plataformas/enUso/scripts/jugador.gd
@@ -21,5 +21,12 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if velocity.x > 0:
+		$AnimatedSprite2D.play("derecha")
+	elif velocity.x < 0:
+		$AnimatedSprite2D.play("izquierda")
+	else:
+		$AnimatedSprite2D.play("default")
 
 	move_and_slide()
