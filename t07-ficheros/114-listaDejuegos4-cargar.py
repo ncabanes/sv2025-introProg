328c5181c2bf9facac87f8233d4060be24d7261b
Nacho Cabanes <ncabanes@users.noreply.github.com>
2026-01-30 22:18:17 +0100

    114 - Lista de juegos + cargar

---
+++ t07-ficheros/114-listaDejuegos4-cargar.py
@@ -0,0 +1,78 @@
+# Lista de juegos
+juegos = []
+
+fichero = open("juegos.txt", "r")
+linea = fichero.readline().rstrip()
+while linea:
+    fragmentos = linea.split("#")
+    
+    juego = fragmentos[0]
+    genero = fragmentos[1]
+    plataforma = fragmentos[2]
+    
+    juegos.append( { "nombre" : juego,
+        "genero" : genero,
+        "plataforma" : plataforma })
+
+    linea = fichero.readline().rstrip()
+fichero.close()
+
+
+terminado = False
+while not terminado:
+
+    print("1- Añadir un nuevo juego")
+    print("2- Editar un juego")
+    print("3- Ver todos los juegos")
+    print("4- Ordenar los juegos")
+    print("0- Salir")
+
+    opcion = input("Qué opcion? ")
+    print()
+
+    if opcion == "1":
+        print("Añadir")
+        juego = input("Dime el juego: ")
+        genero = input("Dime el género: ")
+        plataforma = input("Dime la plataforma: ")
+        juegos.append( { "nombre" : juego,
+            "genero" : genero,
+            "plataforma" : plataforma })
+
+    elif opcion == "2":
+        print("Editar")
+        num_juego = int(input("Número de juego a modificar: "))
+        nuevo_texto = input("Nuevo nombre del juego: ")
+        nuevo_genero = input("Dime el género: ")
+        nueva_plataforma = input("Dime la plataforma: ")
+
+        juegos[ num_juego-1 ] = { "nombre" : nuevo_texto,
+            "genero" : nuevo_genero,
+            "plataforma" : nueva_plataforma }
+
+    elif opcion == "3":
+        print("Ver")
+        for i in range(len(juegos)):
+            print(i+1, juegos[i]["nombre"],
+                "-",juegos[i]["genero"],
+                " (",juegos[i]["plataforma"], ")")
+
+    elif opcion == "4":
+        print("Esta versión aún no puede ordenar")
+        #juegos.sort()
+
+    elif opcion == "0":
+        terminado = True
+        print("Hasta luego")
+
+    else:
+        print("Opción no válida")
+
+fichero = open("juegos.txt", "w")
+for i in range(len(juegos)):
+    fichero.write(juegos[i]["nombre"] + "#")
+    fichero.write(juegos[i]["genero"] + "#")
+    fichero.write(juegos[i]["plataforma"] + "\n")
+fichero.close()
+
+print("Hasta otra!")
