cc554cb8762b5cc1cf76b18acf79818766d034d9
Nacho Cabanes <ncabanes@users.noreply.github.com>
2026-02-24 20:56:26 +0100

    Repaso de funciones 06c: sumar_pares_hasta

---
+++ zRepasos/repaso_f06c2-sumar_pares_hasta2.py
@@ -0,0 +1,18 @@
+"""
+3. Crea una función "sumar_pares_hasta", que devuelva la suma
+de los números pares que hasta el número n, que se indicará
+como parámetro, empezando en 1 (ambos incluidos). Por ejemplo,
+si n = 8, la función debería devolver 20 (porque entre el 1 y
+el 8 están los pares 2, 4, 6, 8, que suman 20). Pruébala desde
+el cuerpo del programa.
+"""
+
+# Versión 2, recorriendo sólo los pares
+
+def sumar_pares_hasta(n: int) -> int:
+    total = 0
+    for i in range(2, n + 1, 2):
+        total += i
+    return total
+
+print(sumar_pares_hasta(8))
