f83d23c95d3de9a7970fa98b2a75c07a17f4c0b3
Nacho Cabanes <ncabanes@users.noreply.github.com>
2026-05-31 14:01:55 +0200

    Repaso para junio: 074 - Comedor (a2: menú con booleano)

---
+++ zRepasos/junio/repaso_junio_074a-comedor1b-menu2-booleano.py
@@ -0,0 +1,30 @@
+# Repaso para Junio 74 (semana del 25/05 al 31/05)
+# 
+# (Este ejercicio es un ejemplo de cómo podría ser el examen final 
+# "grande", valorado de 0 a 10).
+# 
+# El servicio de comedor de un pequeño hospital te ha pedido que les 
+# ayudes a apuntar los menús que van realizando, para evitar repetirse.
+# 
+# Para cada menú desean apuntar el nombre del primer plato (por ejemplo, 
+# "crema de zanahorias"), el nombre del segundo plato (por ejemplo, "lomo 
+# en salsa"), la fecha (como texto, por ejemplo, "2026-02-20") y las 
+# calorías estimadas (un número sin decimales).
+# 
+# El programa debe:
+# 
+# 1. Mostrar un menú que recuerde al usuario todas las opciones 
+# disponibles y se repita hasta que se escoja la opción "A" (Acabar), que 
+# deberá ser aceptable tanto en mayúsculas como en minúsculas. (1 punto) 
+
+terminado = False
+while not terminado:
+    print("1- Añadir menú")
+    print("2- Ver menús entre 2 fechas")
+    print("A- Acabar")
+    opcion = input("Dime una opción: ").upper()
+    
+    if opcion == "A":
+        terminado = True
+
+print("Hasta luego")
