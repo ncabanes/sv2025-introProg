c1afd8da91028c3624b23a7f54e6810aab738fd7
Nacho Cabanes <ncabanes@users.noreply.github.com>
2026-05-31 14:02:33 +0200

    Repaso para junio: 074 - Comedor (c: ver entre dos fechas)

---
+++ zRepasos/junio/repaso_junio_074c-comedor3-ver.py
@@ -0,0 +1,81 @@
+# Repaso para Junio 74 (semana del 25/05 al 31/05)
+# 
+# (Este ejercicio es un ejemplo de cómo podría ser el examen final 
+# "grande", valorado de 0 a 10).
+# 
+# El servicio de comedor de un pequeño hospital te ha pedido que les 
+# ayudes a apuntar los menús que van realizando, para evitar repetirse.
+# 
+# Para cada menú desean apuntar el nombre del primer plato (por ejemplo, 
+# "crema de zanahorias"), el nombre del segundo plato (por ejemplo, "lomo 
+# en salsa"), la fecha (como texto, por ejemplo, "2026-02-20") y las 
+# calorías estimadas (un número sin decimales).
+# 
+# El programa debe:
+# 
+# 1. Mostrar un menú que recuerde al usuario todas las opciones 
+# disponibles y se repita hasta que se escoja la opción "A" (Acabar), que 
+# deberá ser aceptable tanto en mayúsculas como en minúsculas. (1 punto) 
+# 
+# 2. Permitir añadir un nuevo menú, que se guardará en formato de 
+# diccionario, y que a su vez será parte de una lista llamada "menus". 
+# Para cada menú se le pedirá fecha, primer plato, segundo plato y 
+# calorías. Si el segundo plato, por error, se introduce igual que el 
+# primer plato, se deberá volver a pedir, tantas veces como sea 
+# necesario. (2 puntos) 
+# 
+# 3. Poder ver los menús que ha habido entre dos fechas (por ejemplo, 
+# entre  "2026-02-16" y "2026-02-20"). Se pedirá al usuario la fecha 
+# inicial, y la fecha final y se mostrará número, fecha, primer plato y 
+# segundo plato para todos los menús que tengan una fecha comprendida 
+# entre ambas (inclusive). (1 punto)
+
+def anyadir(menus: list) -> list:
+    fecha = input("Dime la fecha del menú (AAAA-MM-DD): ")
+    primero = input("Dime el primer plato del menú: ")
+    segundo = input("Dime el segundo plato del menú: ")
+    while segundo == primero:
+        segundo = input("No debe repetirse. Dime el segundo plato del menú: ")
+    calorias = int(input("Dime las calorías: "))
+    
+    menu = {
+        "fecha": fecha,
+        "primer_plato": primero,
+        "segundo_plato": segundo,
+        "calorias": calorias
+    }
+    menus.append(menu)
+    return menus
+
+
+def ver_entre_dos_fecha(menus: list) -> None:
+    desde = input("Dime desde qué fecha (AAAA-MM-DD): ")
+    hasta = input("Dime hasta qué fecha (AAAA-MM-DD): ")
+    
+    #for menu in menus:
+    for i in range(len(menus)):
+        menu = menus[i]
+        if menu["fecha"] >= desde and menu["fecha"] <= hasta:
+            print(i+1, menu["fecha"],
+                menu["primer_plato"], menu["segundo_plato"])
+
+
+
+# ------------ Cuerpo del programa ----------
+
+menus = [ ]
+terminado = False
+while not terminado:
+    print("1- Añadir menú")
+    print("2- Ver menús entre 2 fechas")
+    print("A- Acabar")
+    opcion = input("Dime una opción: ").upper()
+    
+    if opcion == "1":
+        menus = anyadir(menus)
+    elif opcion == "2":
+        ver_entre_dos_fecha(menus)
+    elif opcion == "A":
+        terminado = True
+
+print("Hasta luego")
